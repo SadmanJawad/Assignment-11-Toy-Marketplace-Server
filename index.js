12999b73a268eee6afaa373e352bd6495f3a9f83
SadmanJawad <jawadnabil977@gmail.com>
2023-05-20 21:43:48 +0600

    server problem fixed

--- index.js
+++ index.js
@@ -19,22 +19,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  
+    // await client.connect();
 
-    const serviceCollection = client.db("toyStore").collection("toys");
+    const toyCollection = client.db("toyStore").collection("toys");
 
     app.get('/toys', async (req, res) => {
-        const cursor = serviceCollection.find();
+        const cursor = toyCollection.find();
         const result = await cursor.toArray();
         res.send(result); 
     })
 
+   
+
+    app.post('/addAToy', async (req, res) => {
+        const newToy = req.body;
+        console.log(newToy)
+        const result = await toyCollection.insertOne(newToy);
+        console.log('got new toy', result)
+        res.send(result)
+    })
 
 
 
